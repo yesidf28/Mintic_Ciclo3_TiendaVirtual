1e07a6737f9254945a9231cb4f4804e9542a0373
Dany Florez <yesid_28@hotmail.com>
2021-09-18 15:35:17 -0500

    inicio del proyecto

---
+++ src/main/java/com/example/tiendaConcesionario/TiendaConcesionarioApplication.java
@@ -0,0 +1,13 @@
+package com.example.tiendaConcesionario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaConcesionarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaConcesionarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tiendaConcesionario/TiendaConcesionarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tiendaConcesionario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaConcesionarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
