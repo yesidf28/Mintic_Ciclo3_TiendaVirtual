049804ea4582b73f296dc9e1d70356c93187e71e
Dany Florez <yesid_28@hotmail.com>
2021-09-18 16:50:20 -0500

    tercer commit

---
+++ src/main/java/com/example/tiendaConcesionario/controller/AppController.java
@@ -0,0 +1,35 @@
+package com.example.tiendaConcesionario.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AppController {
+
+	@GetMapping({"/","/login"})
+	public String index(){
+		return "index";
+	}
+	
+	@GetMapping({"/menu"})
+	public String menu(){
+		return "menu";
+	}
+	
+	@GetMapping({"/user"})
+	public String user(){
+		return "user";
+	}
+	
+	@GetMapping({"/admin"})
+	public String admin(){
+		return "admin";
+	}
+	
+	@GetMapping({"/logout"})
+	public String logout(){
+		return "logout";
+	}
+	
+	
+}
---
+++ src/main/java/com/example/tiendaConcesionario/dao/UsuariosDAO.java
@@ -0,0 +1,10 @@
+package com.example.tiendaConcesionario.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.tiendaConcesionario.model.Usuarios;
+
+public interface UsuariosDAO extends JpaRepository<Usuarios, Integer>{
+
+	
+	
+}
