8c0c6bce5c5ed699785b17daba91c5c0b42b0ceb
Dany Florez <yesid_28@hotmail.com>
2021-09-25 18:16:28 -0500

    commit login

--- src/main/java/com/example/tiendaConcesionario/controller/AppController.java
+++ src/main/java/com/example/tiendaConcesionario/controller/AppController.java
@@ -1,7 +1,10 @@
 package com.example.tiendaConcesionario.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AppController {
@@ -29,6 +32,21 @@ public class AppController {
 	@GetMapping({"/logout"})
 	public String logout(){
 		return "logout";
+	}
+	
+	@RequestMapping("/validador")
+	public String validador(HttpServletRequest req, HttpServletRequest resp) {
+		
+		String usuario = req.getParameter("usuario");
+		String password = req.getParameter("password");	
+		
+		
+		if(usuario.equals("admin") && password.equals("12345")) {
+			return "menu";
+		}else {			
+			return "index";
+		}
+		
 	}
 	
 	
